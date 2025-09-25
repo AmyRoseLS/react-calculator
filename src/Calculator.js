236d8c5f0e5c5bda33132cee0fb37821850e6921
Amy-Rose <amyrlcollins@gmail.com>
2025-09-25 14:59:37 +0100

    handles addition, subtraction and division. multiplication currently broken for some reason

--- src/Calculator.js
+++ src/Calculator.js
@@ -11,16 +11,14 @@ export default function Calculator() {
   let calc; //eeeee check with someone about how you're declaring this! It will be a match object later.
 
 
-  function doMath (screen,label) {
-    console.log(`doMath got called`);
-
+  function doMath (screen) {
     // tokenise & label screen string 
-    // that means: break string into an array that looks like [ {number:12}, {operator:+}, {number:34} ]
+    // that means: break string into an array that looks like, e.g. [ 12, {operator:1}, 34 ]
     calc=screen.match(/\d+|[+\-*/]/g).map(token => {
       if(/^\d+$/.test(token)){
-        return {num:Number(token)};
+        return Number(token);
       } else {
-        // storing the operator as their place in the precedence order BIDMAS
+        // storing the operator as its place in the precedence order BIDMAS
         switch(token) {
           case "/":
             return {op:1};
@@ -33,12 +31,51 @@ export default function Calculator() {
         }
       };
     })
-    console.log(`calc looks like`);
-    console.log(calc);
 
-    return "uwu";
+   // do the math!
+   let precedence = 1;
+
+   const basicOps = {
+     // links precedence number to the operation
+      "1": (a,b) => a/b,
+      "2": (a,b) => a*b,
+      "3": (a,b) => a+b,
+      "4": (a,b) => a-b
+    };
+
+    const numOfBasicOps = Object.keys(basicOps).length; // equivalent to the number of operators the calculator can currently handle
+   
+    let index;
+    let result;
+
+    // loop through the calc array doing the math in precedence order
+    while (calc.length>1) {
+      console.log('in first while loop');
+      while (precedence <= numOfBasicOps){ //change 4 to basicOps.length
+        console.log('in second while loop');
+        index = calc.findIndex( i=> i.op == precedence)
+        while (index>-1) {
+          console.log('in third while loop');
+          result = basicOps[precedence.toString()](calc[index-1], calc[index+1])
+          
+          // replace inner calculation with the result in calc
+          calc = [...calc.slice(0,index-1),
+            result,
+            ...calc.slice(index+2)
+          ];
+
+          //check for another inner calculation with the current precedence
+          index = calc.findIndex( i => i.op == precedence)
+
+          console.log(calc);
+        }
+      precedence += 1;
+      }  
+    }
+    return calc[0].toString();
   }
 
+  
   function handleClick(label,func) {
      switch (func) {
       case 'back':
@@ -53,7 +90,7 @@ export default function Calculator() {
         setScreen(newScreen);
         break;
       case 'equals':
-        let answer = doMath(screen,label);
+        let answer = doMath(screen);
         setScreen(answer);
         break;
     }
